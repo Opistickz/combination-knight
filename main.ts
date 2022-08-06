733cac887db06fd7eabb741a5ebcb836f2c2ad60
Opistickz <110703278+Opistickz@users.noreply.github.com>
2022-08-05 21:10:02 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,36 +9,80 @@ controller.player1.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pres
         noj += 1
     }
 })
+blockMenu.onMenuOptionSelected(function (option, index) {
+    if (option == "Play") {
+        scene.setBackgroundColor(9)
+        tiles.setCurrentTilemap(tilemap`level1`)
+        stats.turnStats(true)
+        Ck1 = sprites.create(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `, SpriteKind.Player)
+        tiles.placeOnRandomTile(Ck1, assets.tile`myTile`)
+        controller.player1.moveSprite(Ck1, 100, 0)
+        scene.cameraFollowSprite(Ck1)
+        Ck1.ay = 150
+        noj = 0
+        Max_Jumps = 2
+    }
+})
+let Ck1: Sprite = null
 let Max_Jumps = 0
 let noj = 0
-let Ck1: Sprite = null
-scene.setBackgroundColor(9)
-tiles.setCurrentTilemap(tilemap`level1`)
-stats.turnStats(true)
-Ck1 = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-tiles.placeOnRandomTile(Ck1, assets.tile`myTile`)
-controller.player1.moveSprite(Ck1, 100, 0)
-scene.cameraFollowSprite(Ck1)
-Ck1.ay = 150
-noj = 0
-Max_Jumps = 2
+blockMenu.showMenu(
+["Play", "Spells"],
+0,
+MenuLocation.BottomHalf,
+[img`
+    f . . . . . . . . . . . . . . . 
+    f f f . . . . . . . . . . . . . 
+    f f f f f . . . . . . . . . . . 
+    f f f f f f f . . . . . . . . . 
+    f f f f f f f f f . . . . . . . 
+    f f f f f f f f f f f . . . . . 
+    f f f f f f f f f f f f f . . . 
+    f f f f f f f f f f f f f f f . 
+    f f f f f f f f f f f f f f f . 
+    f f f f f f f f f f f f f . . . 
+    f f f f f f f f f f f . . . . . 
+    f f f f f f f f f . . . . . . . 
+    f f f f f f f . . . . . . . . . 
+    f f f f f . . . . . . . . . . . 
+    f f f . . . . . . . . . . . . . 
+    f . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . b . . . . . . . 
+    . . . . . . . b d b . . . . . . 
+    . . . . . . . c d c . . . . . . 
+    . . . . . . . c 5 c . . . . . . 
+    . . . . . . c d 5 d c . . . . . 
+    . . . b c c d 5 5 5 d c c b . . 
+    . . b d d 5 5 5 5 5 5 5 d d b . 
+    . . . b c c d 5 5 5 d c c b . . 
+    . . . . . . c d 5 d c . . . . . 
+    . . . . . . . c 5 c . . . . . . 
+    . . . . . . . c d c . . . . . . 
+    . . . . . . . b d b . . . . . . 
+    . . . . . . . . b . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `]
+)
 forever(function () {
 	
 })
